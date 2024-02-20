12216afe504f8ac7c9d1e3a8ea2ed8315c6ac5fb
Tamtee247 <tamleonard1@gmail.com>
2024-02-20 12:47:48 -0500

    upload

--- IndexResponse5.java
+++ IndexResponse5.java
@@ -10,10 +10,11 @@ public class IndexResponse5 {
     @Test
     public void index5() {
         RestAssured.baseURI="https://reqres.in/api/users";
-        Response response = (Response) given().queryParam("name", "Charles").header("Content-Type","application/json");
-        when().get("v1/search");
-        String name1=response.jsonPath().getString("result[5].name");
-        System.out.println("name1 ="+name1);
+        Response response = (Response) given().queryParam
+                ("total_pages", "2").header("Content-Type","application/json");
+        when().get("api/users");
+        String page1=response.jsonPath().getString("total[2].pages");
+        System.out.println("page1 ="+page1);
 }
 }
 

960bab7ee82a7a4357c810e4f20a7944826dbec4
Tamtee247 <tamleonard1@gmail.com>
2024-02-21 13:55:04 -0500

    upload

--- ValueofBookingId7.java
+++ ValueofBookingId7.java
@@ -3,12 +3,14 @@ package Jan2024Api;
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class ValueofBookingId7 {
 
     @Test
     public void firstbookingid(){
-        RestAssured.baseURI="https://restful-booker.herokuapp.com/;
-        String firstbookingid = RestAssured.given().queryParam("id").when().get().then().statusCode(200)
+        RestAssured.baseURI="https://restful-booker.herokuapp.com/";
+                RestAssured.given().queryParam("id").when().get().then().statusCode(200)
                 .extract().jsonPath().getString("id");
         System.out.println("id");
     }

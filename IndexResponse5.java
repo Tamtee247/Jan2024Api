6cfd389ccc9780889cd207f65ae40c0f94691bb8
Tamtee247 <tamleonard1@gmail.com>
2024-02-20 19:52:17 -0500

    upload

--- IndexResponse5.java
+++ IndexResponse5.java
@@ -9,11 +9,11 @@ import static io.restassured.RestAssured.*;
 public class IndexResponse5 {
     @Test
     public void index5() {
-        RestAssured.baseURI="https://reqres.in/api/users";
+        RestAssured.baseURI="https://restful-booker.herokuapp.com/";
         Response response = (Response) given().queryParam
-                ("total_pages", "2").header("Content-Type","application/json");
-        when().get("api/users");
-        String page1=response.jsonPath().getString("total[2].pages");
+                ("viewport", "width=device-width, initial-scale=1").header("Content-Type","text/html; charset=UTF-8");
+        when().get("herokuapp.com/");
+        String page1=response.htmlPath().getString("total[1].pages");
         System.out.println("page1 ="+page1);
 }
 }

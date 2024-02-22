ee8be0a68d57c14b0dc964243ce93b6d762d4fee
Tamtee247 <tamleonard1@gmail.com>
2024-02-21 19:52:30 -0500

    upload

--- Index5.java
+++ Index5.java
@@ -11,11 +11,11 @@ public class Index5 {
 
     @Test
     public void ResultsofIndex5() {
-        RestAssured.baseURI="https://restful-booker.herokuapp.com/";
+        RestAssured.baseURI="https://reqres.in/api/users";
         Response response = (Response) given().queryParam
-                ("rel","stylesheet").header("Content-Type","text/html; charset=UTF-8");
-        String viewport = null;
-        System.out.println("viewport ="+viewport);
+                ("data.id","1").header("Content-Type","application/json; charset=UTF-8");
+        String data1 = null;
+        System.out.println("data.1 ="+data1);
 }
 }
 

7da085b7cb122145300f05a7399639906a97410c
Tamtee247 <tamleonard1@gmail.com>
2024-02-22 14:33:31 -0500

    upload

--- Fetchthe5thindex.java
+++ Fetchthe5thindex.java
@@ -12,14 +12,14 @@ public class Fetchthe5thindex {
 
     @Test
     public void ResultsofIndex5() {
-        RestAssured.baseURI="https://reqres.in/api/users";
+        baseURI="https://reqres.in/api/users/2";
         Response response =
-                (Response) given().queryParam("data.id")
+                (Response) given().queryParam("data.last_name")
                 .header("Content-Type","application/json; charset=UTF-8");
         when().get("api/users");
         System.out.println(" response string =" + response.getBody().asString());
-        String data1 =response.jsonPath().getString("data[1].id");
-        System.out.println("data1.id ="+data1);
+        String data1 = response.jsonPath().getString("data[1].last_name");
+        System.out.println("data.last_name=" + data1.indexOf(2));
 }
 }
 

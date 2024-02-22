6ef86b0226345f5267f31ebf4f811e0fd5776b1d
Tamtee247 <tamleonard1@gmail.com>
2024-02-22 18:38:41 -0500

    upload

--- Fetchthe5thindex.java
+++ Fetchthe5thindex.java
@@ -4,22 +4,20 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-
-
 import static io.restassured.RestAssured.*;
 
 public class Fetchthe5thindex {
 
     @Test
     public void ResultsofIndex5() {
-        baseURI="https://reqres.in/api/users/2";
+        RestAssured.baseURI="https://reqres.in/api/users/2";
         Response response =
-                (Response) given().queryParam("data.last_name")
+                (Response) given().queryParam("name","Janet")
                 .header("Content-Type","application/json; charset=UTF-8");
-        when().get("api/users");
-        System.out.println(" response string =" + response.getBody().asString());
-        String name1=response.jsonPath().getString("result[1].name");
-        System.out.println("name1 ="+name1);
+        when().get("/api/users/");
+        System.out.println(" response string" + response.getBody().asString());
+        String name1 =response.jsonPath().getString("Janet");
+        System.out.println("name1=" +name1);
 }
 }
 

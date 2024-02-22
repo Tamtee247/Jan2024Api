6437c3aa8044cd61b86dd83f0b1b1f096c9ed6dd
Tamtee247 <tamleonard1@gmail.com>
2024-02-21 19:00:38 -0500

    upload

--- Index5.java
+++ Index5.java
@@ -2,14 +2,13 @@ package Jan2024Api;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.Test;
+
 
 import static io.restassured.RestAssured.*;
 
 public class Index5 {
 
-    @Test
-    public void Index5() {
+    public void ResultsofIndex5() {
         RestAssured.baseURI="https://restful-booker.herokuapp.com/";
         Response response = (Response) given().queryParam
                 ("stylesheet").header("Content-Type","text/html; charset=UTF-8");

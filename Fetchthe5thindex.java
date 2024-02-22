7adbb8e58cf50243b4abefd22b9ec5a37538bd9e
Tamtee247 <tamleonard1@gmail.com>
2024-02-22 13:19:25 -0500

    upload

--- Fetchthe5thindex.java
+++ Fetchthe5thindex.java
@@ -13,12 +13,13 @@ public class Fetchthe5thindex {
     @Test
     public void ResultsofIndex5() {
         RestAssured.baseURI="https://reqres.in/api/users";
-        Response response = (Response) given().queryParam
-                ("data.id","1").header("Content-Type","application/json; charset=UTF-8");
+        Response response =
+                (Response) given().queryParam("data.id")
+                .header("Content-Type","application/json; charset=UTF-8");
         when().get("api/users");
         System.out.println(" response string =" + response.getBody().asString());
-        String data1 =response.jsonPath().getString("data.id");
-        System.out.println("data.id ="+data1);
+        String data1 =response.jsonPath().getString("data[1].id");
+        System.out.println("data1.id ="+data1);
 }
 }
 
--- StatusCode3.java
+++ StatusCode3.java
@@ -1,13 +1,11 @@
 package Jan2024Api;
 
 import io.restassured.response.Response;
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
-public class ResponseStatusCode3 {
+public class StatusCode3 {
 
         @Test
         public void statuscode201(){

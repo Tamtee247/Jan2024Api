9019188c7076391dbfdf025d7293dd973df3fddb
Tamtee247 <tamleonard1@gmail.com>
2024-02-26 10:01:06 -0500

    upload

--- Valueofquest5.java
+++ Valueofquest5.java
@@ -1,23 +1,27 @@
 package Jan2024Api;
 
 
-import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+
 
 
 public class Valueofquest5 {
 
     @Test
     public void valueofquest5() {
-        RestAssured.baseURI="https://reqres.in/api/users";
-        Response response = (Response) given().queryParam("name","Charles").header("Content-Type","application/json");
-        when().get("/v1/search"); System.out.println("string id =" + response.getBody().asString());
-        String name5 =response.jsonPath().getString("results[5].name");
-        System.out.println("name5 = " + name5);
+        HashMap<String,String> reqBody = new HashMap<String,String>();
+        reqBody.get("id =");
+        reqBody.get("first_name");
+        reqBody.get("Charles");
+        Response res=given().contentType(ContentType.JSON).body(reqBody).when().post("https://reqres.in/api/users");
+        System.out.println("get map body = " +res.getBody().asString());
+        System.out.println("contentType =" + res.contentType());
 
 
 

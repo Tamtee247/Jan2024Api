7acb98740d7ae0a59d37d84d2063c72498b7c60f
Tamtee247 <tamleonard1@gmail.com>
2024-02-26 10:30:58 -0500

    upload

--- Valueofquest5.java
+++ Valueofquest5.java
@@ -2,11 +2,9 @@ package Jan2024Api;
 
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -15,7 +13,7 @@ import static io.restassured.RestAssured.when;
 public class Valueofquest5 {
 
     @Test
-    public void valueofquest5() {
+    public void indexofquest5() {
         RestAssured.baseURI="https://restful-booker.herokuapp.com/";
         Response response = (Response) given().queryParam("name","Charles").header("Content-Type","application/json");
         when().get("v1/search");
